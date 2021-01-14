ba25d594f6dffdf6b84a53b9c17deeb9dfa49102
imhamed <hmdmohebbi@gmail.com>
2021-01-14 17:41:58 +1100

    init app and cleaning

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+import SafePage from "./SafePage";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Container fluid className="p-0">
+        <SafePage />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SafePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SafePage() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default SafePage
