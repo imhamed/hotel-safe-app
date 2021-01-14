772ab4490d280b70408bfe518d026d22d9e751da
imhamed <hmdmohebbi@gmail.com>
2021-01-14 18:38:00 +1100

    CLR and ENT click handlers and lock/unlock logics

--- src/components/SafePage.js
+++ src/components/SafePage.js
@@ -6,8 +6,9 @@ import Indicator from "./Indicator";
 import "./SafePage.css";
 
 function SafePage() {
-    
   const [pin, setPin] = useState("");
+  const [correctPin, setCorrectPin] = useState("");
+  const [unlock, setUnlock] = useState(true);
 
   function handleNum(e) {
     const num = pin;
@@ -15,12 +16,45 @@ function SafePage() {
       setPin(num + e.target.id);
     }
   }
+  function handleCLR() {
+    setPin("");
+  }
+
+  function handleENT() {
+    if (unlock) {
+      lockSafe();
+    } else {
+      unlockSafe();
+    }
+    setPin("");
+  }
+  function lockSafe() {
+    if (pin.length === 4) {
+      setCorrectPin(pin);
+      setUnlock(false);
+    } else {
+    }
+  }
+
+  function unlockSafe() {
+    if (pin.length === 4) {
+      if (pin === correctPin) {
+        setUnlock(true);
+      } else {
+      }
+    } else {
+    }
+  }
 
   return (
     <Container className="main-container">
       <Row>
         <Col>
-          <Keyboard handleNum={handleNum} />
+          <Keyboard
+            handleNum={handleNum}
+            handleCLR={handleCLR}
+            handleENT={handleENT}
+          />
         </Col>
         <Col>
           <Row>
