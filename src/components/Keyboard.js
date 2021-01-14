ca9cb02db7868b1a72385a602ed0124bf8e03513
imhamed <hmdmohebbi@gmail.com>
2021-01-14 18:24:02 +1100

    num button click handle

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,37 +1,35 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
-
-function Keyboard() {
+function Keyboard({handleNum, handleCLR, handleENT}) {
   return (
     <Container>
       <table>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
+            <td id="1" onClick={handleNum}>1</td>
+            <td id="2" onClick={handleNum}>2</td>
+            <td id="3" onClick={handleNum}>3</td>
           </tr>
           <tr>
-            <td>4</td>
-            <td>5</td>
-            <td >6</td>
+            <td id="4" onClick={handleNum}>4</td>
+            <td id="5" onClick={handleNum}>5</td>
+            <td id="6" onClick={handleNum}>6</td>
           </tr>
           <tr>
-            <td >7</td>
-            <td >8</td>
-            <td >9</td>
+            <td id="7" onClick={handleNum}>7</td>
+            <td id="8" onClick={handleNum}>8</td>
+            <td id="9" onClick={handleNum}>9</td>
           </tr>
           <tr>
-            <td >CLR</td>
-            <td >0</td>
-            <td >ENT</td>
+            <td id="CLR" onClick={handleCLR}>CLR</td>
+            <td id="0" onClick={handleNum}>0</td>
+            <td id="ENT" onClick={handleENT}>ENT</td>
           </tr>
         </tbody>
       </table>
     </Container>
   );
 }
-
 
 export default Keyboard;
--- src/components/SafePage.js
+++ src/components/SafePage.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Display from "./Display";
 import Keyboard from "./Keyboard";
 import Indicator from "./Indicator";
 import "./SafePage.css";
 
-
 function SafePage() {
+    
+  const [pin, setPin] = useState("");
+
+  function handleNum(e) {
+    const num = pin;
+    if (pin.length < 4) {
+      setPin(num + e.target.id);
+    }
+  }
+
   return (
-<Container className="main-container">
+    <Container className="main-container">
       <Row>
         <Col>
-          <Keyboard
-          />
+          <Keyboard handleNum={handleNum} />
         </Col>
         <Col>
           <Row>
